dc78b6cdc2d6e92d2321c2ac26c4f6d8e296a771
hklamsir <hklamsir@gmail.com>
2026-01-27 22:09:47 +0800

    Add admin API CSRF protection

--- api/admin/cleanup_orphan_images.php
+++ api/admin/cleanup_orphan_images.php
@@ -3,6 +3,7 @@
 
 require_once __DIR__ . '/../../includes/db.php';
 require_once __DIR__ . '/../../includes/admin_check.php';
+require_once __DIR__ . '/../../includes/csrf_check.php';
 require_once __DIR__ . '/../../includes/api_response.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
